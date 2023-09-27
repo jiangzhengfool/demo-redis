58d11a531c34a9f89fad18f0d329aef5841c76d0
jiangzhengfu <jiangzhengfu@mchz.com.cn>
2023-09-27 17:43:57 +0800

    空项目

--- src/main/java/com/example/demoredis/service/RedisCache.java
+++
@@ -1,8 +0,0 @@
-package com.example.demoredis.service;
-
-import com.github.benmanes.caffeine.cache.Cache;
-
-public interface RedisCache extends Cache<Object, Object> {
-    public int ttl = 0;
-
-}
