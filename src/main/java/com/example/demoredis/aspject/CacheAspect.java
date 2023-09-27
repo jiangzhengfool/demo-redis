c041cca97d3023d07fb20f1492281ef28a091067
jiangzhengfu <jiangzhengfu@mchz.com.cn>
2023-09-27 13:29:56 +0800

    epc插件集群

--- src/main/java/com/example/demoredis/aspject/CacheAspect.java
+++ src/main/java/com/example/demoredis/aspject/CacheAspect.java
@@ -6,11 +6,17 @@ import com.example.demoredis.enums.CacheType;
 import com.example.demoredis.util.ElParser;
 import com.github.benmanes.caffeine.cache.Cache;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
@@ -19,6 +25,8 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
@@ -34,14 +42,42 @@ public class CacheAspect  {
 
     @Resource(name = "caffeineCache")
     private Cache cache;
-    @Resource
+
     private  RedisTemplate<String, Object> redisTemplate;
 
     @PostConstruct
-    void init(){
-        // 否则会使用默认的序列化，导致key出现一些乱码前缀
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    void init() {
+//        // 否则会使用默认的序列化，导致key出现一些乱码前缀
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+////        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        GenericObjectPoolConfig redisPool = new GenericObjectPoolConfig();
+
+        Map<String, Object> source = new HashMap<>(8);
+        source.put("spring.redis.cluster.nodes", "100.122.160.132:6379,100.122.160.132:6380,100.122.160.132:6381");
+        RedisClusterConfiguration secondaryRedisClusterConfig;
+        secondaryRedisClusterConfig = new RedisClusterConfiguration(new MapPropertySource("RedisClusterConfiguration", source));
+//		redisClusterConfiguration.setPassword(environment.getProperty("spring.redis.password"));
+
+        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder().poolConfig(redisPool).build();
+
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(secondaryRedisClusterConfig, clientConfiguration);
+        lettuceConnectionFactory.afterPropertiesSet();
+
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        template.afterPropertiesSet();
+        redisTemplate = template;
     }
 
 
